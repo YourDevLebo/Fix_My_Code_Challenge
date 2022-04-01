f354495fc32e08efa3d42d0eabfd1ca577b2e6df
YourDevLebo <djlebzana@gmail.com>
2022-04-01 12:35:50 +0200

    fixed square

---
+++ square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This module containing an object square """
+
+
+class square():
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize square"""
+        for key, value in kwargs.items():
+            if value > 0:
+                setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Perimeter of the square """
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """ String representation of the square """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
