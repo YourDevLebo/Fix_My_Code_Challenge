b554b8d54ed06eab5d3106ac3faa2fd7f522f9b2
YourDevLebo <djlebzana@gmail.com>
2022-04-01 12:41:39 +0200

    fixed class name

--- square.py
+++ square.py
@@ -2,7 +2,7 @@
 """ This module containing an object square """
 
 
-class square():
+class Square():
 
     width = 0
     height = 0
@@ -28,7 +28,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
